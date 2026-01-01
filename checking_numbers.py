c72685e99cb3557b28bfc129af6a73730eb119d7
HVamshikrishna <vhanumap@student.gitam.edu>
2026-01-01 14:59:54 +0530

    Create checking_numbers.py

---
+++ checking_numbers.py
@@ -0,0 +1,11 @@
+Number = 26
+ 
+if Number < 0:
+ print(f"The number {Number} is negative ")
+
+elif Number > 0:
+ print(f"The number {Number} is positive")
+else:
+ print(f"The number {Number}is zero")
+##output 
+The number 26 is positive
