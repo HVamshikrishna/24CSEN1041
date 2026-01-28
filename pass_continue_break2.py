31e4a6d8314912f43fa8cc32f1aefef4f561d90b
HVamshikrishna <vhanumap@student.gitam.edu>
2026-01-28 10:47:02 +0530

    Create pass_continue_break2.py

---
+++ pass_continue_break2.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,100,15,16,17,18,19,20]
+
+for n in numbers:
+    if n == 100:
+        print("Error: Found invalid data (100). Aborting!")
+        break  # STOP everything
+    
+    elif n % 2 == 0:
+        continue  # SKIP even numbers
+        
+    elif n == 1:
+        pass      # Do NOTHING special for 1, just let it proceed
+        
+    print(f"odd number: {n}")
+  ##output
+odd number: 1
+odd number: 3
+odd number: 5
+odd number: 7
+odd number: 9
+odd number: 11
+odd number: 13
+Error: Found invalid data (100). Aborting!
