a349b0aa76a2c1e979e228ae09fabed4eaa37f84
HVamshikrishna <vhanumap@student.gitam.edu>
2026-01-29 15:38:32 +0530

    Create operators3.py

---
+++ operators3.py
@@ -0,0 +1,15 @@
+x = 10
+y = 3
+
+# 1. Arithmetic Operators
+print("--- Arithmetic Operators ---")
+# %d is a placeholder for an Integer (Digit)
+print("%d + %d = %d" % (x, y, x + y))
+print("%d - %d = %d" % (x, y, x - y))
+print("%d * %d = %d" % (x, y, x * y))
+
+##output
+ Arithmetic Operators 
+10 + 3 = 13
+10 - 3 = 7
+10 * 3 = 30
