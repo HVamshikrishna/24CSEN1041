4a582f94726f4109966f537369f936b5bda76cd4
HVamshikrishna <vhanumap@student.gitam.edu>
2026-02-05 15:02:09 +0530

    Create inbuilt_func.py

---
+++ inbuilt_func.py
@@ -0,0 +1,22 @@
+import math
+numbers = [10, 20, 5, 40]
+
+count = len(numbers)
+highest = max(numbers)
+
+text_num = "100"
+real_num = int(text_num)
+
+print(f"Count: {count}, Max: {highest}, Converted: {real_num}")
+
+root = math.sqrt(1000)
+pi_val = math.pi 
+pi_val2 = math.pi * 3
+print(root)
+print(pi_val2)
+print(pi_val)
+##output
+Count: 4, Max: 40, Converted: 100
+31.622776601683793
+9.42477796076938
+3.141592653589793
